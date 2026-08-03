736a3216d703413e52903134c1c4e33c974783f9
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-08-03 12:05:20 +0530

    release: Check certificate thumbprint.

--- src/InstallerService.cs
+++ src/InstallerService.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Security;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Management.Deployment;
+using static System.StringComparison;
 using static Windows.Foundation.AsyncStatus;
 
 namespace Flarial.Launcher.Installer;
@@ -17,25 +20,33 @@ static class InstallerService
         ForceUpdateFromAnyVersion = true
     };
 
-    const string PackageUri = "https://cdn.flarial.xyz/launcher/Flarial.Launcher.msix";
-    const string CertificateUri = "https://cdn.flarial.xyz/launcher/Flarial.Launcher.cer";
+    const string LauncherPackageUri = "https://cdn.flarial.xyz/launcher/Flarial.Launcher.msix";
+    const string LauncherCertificateUri = "https://cdn.flarial.xyz/launcher/Flarial.Launcher.cer";
+    const string LauncherCertificateThumbprint = "080862035B63C6B01A1F7F5E2A286939808F502ADCA100BDCB6F805FB0DD4171";
 
     internal static Task InstallCertificateAsync() => Task.Run(static async () =>
     {
-        using X509Store store = new(StoreName.TrustedPeople, StoreLocation.LocalMachine);
-        using X509Certificate2 certificate = new(await HttpService.GetBytesAsync(CertificateUri));
+        using X509Store certificateStore = new(StoreName.TrustedPeople, StoreLocation.LocalMachine);
+        certificateStore.Open(OpenFlags.ReadWrite);
 
-        store.Open(OpenFlags.ReadWrite);
+        foreach (var installedCertificate in certificateStore.Certificates)
+        {
+            var installedCertificateThumbprint = installedCertificate.GetCertHashString(HashAlgorithmName.SHA256);
+            if (LauncherCertificateThumbprint.Equals(installedCertificateThumbprint, OrdinalIgnoreCase)) return;
+        }
 
-        if (store.Certificates.Find(X509FindType.FindByThumbprint, certificate.Thumbprint, false).Count > 0)
-            return;
+        using X509Certificate2 launcherCertificate = new(await HttpService.GetBytesAsync(LauncherCertificateUri));
+        var launcherCertificateFingerprint = launcherCertificate.GetCertHashString(HashAlgorithmName.SHA256);
 
-        store.Add(certificate);
+        if (!launcherCertificateFingerprint.Equals(LauncherCertificateThumbprint, OrdinalIgnoreCase))
+            throw new("The launcher's remote thumbprint doesn't match the installer's local thumbprint.");
+
+        certificateStore.Add(launcherCertificate);
     });
 
     internal static Task InstallPackageAsync(Action<int> callback) => Task.Run(() =>
     {
-        var info = s_manager.AddPackageByUriAsync(new(PackageUri), s_options);
+        var info = s_manager.AddPackageByUriAsync(new(LauncherPackageUri), s_options);
         try
         {
             using ManualResetEventSlim handle = new();
